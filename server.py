8a6c348f529905274ead0cb1dabe6c9ed963796f
AlminPiricDojo <88908542+AlminPiricDojo@users.noreply.github.com>
2023-09-06 21:14:37 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask_app.controllers import user_controller
+from flask_app.controllers import user_controller, item_controller
 from flask_app import app
           
 if __name__ == "__main__":
